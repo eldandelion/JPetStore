b562e85ad695f58aa4bee74447b5dd9b62ec7ee4
caiyayun <3085018791@qq.com>
2024-01-06 16:28:15 +0800

    account insert changed

--- src/main/java/com/danyayun/jpetstore/persistence/impl/AccountDaoImpl.java
+++ src/main/java/com/danyayun/jpetstore/persistence/impl/AccountDaoImpl.java
@@ -27,7 +27,7 @@ public class AccountDaoImpl implements AccountDao {
 
     private static final String GETACCOUNTBYUSERNAME = "SELECT SIGNON.USERNAME,ACCOUNT.EMAIL,ACCOUNT.FIRSTNAME,ACCOUNT.LASTNAME,ACCOUNT.STATUS,ACCOUNT.ADDR1 AS address1,ACCOUNT.ADDR2 AS address2,ACCOUNT.CITY,ACCOUNT.STATE,ACCOUNT.ZIP,ACCOUNT.COUNTRY,ACCOUNT.PHONE,PROFILE.LANGPREF AS languagePreference,PROFILE.FAVCATEGORY AS favouriteCategoryId,PROFILE.MYLISTOPT AS listOption,PROFILE.BANNEROPT AS bannerOption,BANNERDATA.BANNERNAME FROM ACCOUNT, PROFILE, SIGNON, BANNERDATA WHERE ACCOUNT.USERID =? AND SIGNON.USERNAME = ACCOUNT.USERID AND PROFILE.USERID = ACCOUNT.USERID AND PROFILE.FAVCATEGORY = BANNERDATA.FAVCATEGORY";
     //private static final String GET_ACCOUNT_BY_USERNAME_AND_PASSWORD = "SELECT SIGNON.USERNAME, ACCOUNT.EMAIL, ACCOUNT.FIRSTNAME, ACCOUNT.LASTNAME, ACCOUNT.STATUS, ACCOUNT.ADDR1 AS address1, ACCOUNT.ADDR2 AS address2, ACCOUNT.CITY, ACCOUNT.STATE, ACCOUNT.ZIP, ACCOUNT.COUNTRY, ACCOUNT.PHONE, PROFILE.LANGPREF AS languagePreference, PROFILE.FAVCATEGORY AS favouriteCategoryId, PROFILE.MYLISTOPT AS listOption, PROFILE.BANNEROPT AS bannerOption, BANNERDATA.BANNERNAME    FROM ACCOUNT, PROFILE, SIGNON, BANNERDATA    WHERE ACCOUNT.USERID = ? AND SIGNON.PASSWORD = ? AND SIGNON.USERNAME = ACCOUNT.USERID AND PROFILE.USERID = ACCOUNT.USERID AND PROFILE.FAVCATEGORY = BANNERDATA.FAVCATEGORY";
-    private static final String INSERTACCOUNT = "INSERT INTO ACCOUNT(EMAIL, FIRSTNAME, LASTNAME, STATUS, ADDR1, ADDR2, CITY,STATE,ZIP,COUNTRY,PHONE,USERID)VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
+    private static final String INSERTACCOUNT = "INSERT INTO ACCOUNT(USERID,EMAIL, FIRSTNAME, LASTNAME, STATUS, ADDR1, ADDR2, CITY,STATE,ZIP,COUNTRY,PHONE)VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
     private static final String INSERTPROFILE = "INSERT INTO PROFILE (LANGPREF, FAVCATEGORY, MYLISTOPT, BANNEROPT, USERID)    VALUES (?, ?, ?, ?, ?)";
     private static final String INSERTSIGNON = "INSERT INTO SIGNON (USERNAME,PASSWORD) VALUES (?, ?)";
     private static final String UPDATEACCOUNT = "UPDATE ACCOUNT SET EMAIL = ?,FIRSTNAME = ?,LASTNAME = ?,STATUS = ?,ADDR1 = ?,ADDR2 = ?,CITY = ?,STATE = ?,ZIP = ?,COUNTRY = ?,PHONE = ? WHERE USERID = ?";
@@ -121,18 +121,18 @@ public class AccountDaoImpl implements AccountDao {
         try{
             Connection connection = DBUtil.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(INSERTACCOUNT);
-            preparedStatement.setString(1,account.getEmail());
-            preparedStatement.setString(2,account.getFirstName());
-            preparedStatement.setString(3,account.getLastName());
-            preparedStatement.setString(4,account.getStatus());
-            preparedStatement.setString(5,account.getAddress1());
-            preparedStatement.setString(6,account.getAddress2());
-            preparedStatement.setString(7,account.getCity());
-            preparedStatement.setString(8,account.getState());
-            preparedStatement.setString(9,account.getZip());
-            preparedStatement.setString(10,account.getCountry());
-            preparedStatement.setString(11,account.getPhone());
-            preparedStatement.setString(12,account.getUsername());
+            preparedStatement.setString(1,account.getUsername());
+            preparedStatement.setString(2,account.getEmail());
+            preparedStatement.setString(3,account.getFirstName());
+            preparedStatement.setString(4,account.getLastName());
+            preparedStatement.setString(5,account.getStatus());
+            preparedStatement.setString(6,account.getAddress1());
+            preparedStatement.setString(7,account.getAddress2());
+            preparedStatement.setString(8,account.getCity());
+            preparedStatement.setString(9,account.getState());
+            preparedStatement.setString(10,account.getZip());
+            preparedStatement.setString(11,account.getCountry());
+            preparedStatement.setString(12,account.getPhone());
             preparedStatement.executeUpdate();
             DBUtil.closePreparedStatement(preparedStatement);
             DBUtil.closeConnection(connection);
